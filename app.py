d6a95e271525ed6e364503c4323618ef49ea52f1
Peter Olaomi <p.olaomi@gmail.com>
2023-04-04 17:47:32 -0400

    Adding all the atrributes to app.py file

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import sklearn
 from dataframe_selector import DataFrameSelector
 from categorical_encoder import CategoricalEncoder
 from attributes_adder import AttributesAdder
+from sklearn.preprocessing import OneHotEncoder
 #from django.shortcuts import render
 
 from sklearn.base import BaseEstimator, TransformerMixin 
@@ -17,7 +18,36 @@ __main__.CategoricalEncoder= CategoricalEncoder
 __main__.AttributesAdderr = AttributesAdder
 
     
-
+class AttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self,add_bedrooms_per_room=True):
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X, y=None):
+        rooms_per_household = X[:, 3] / X[:,6]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:,4]/ X[:,3]
+            return np.c_[X,rooms_per_household, bedrooms_per_room]
+        else:
+            return np.c_[X,rooms_per_household]
+class CategoricalEncoder(BaseEstimator, TransformerMixin):
+    def __init__(self, ohe = OneHotEncoder()):
+        self.ohe = ohe
+    def fit(self, X,y=None,):
+        return self.ohe.fit(X)
+    def transform(self, X, y=None):
+        return self.ohe.transform(X).toarray()
+    
+class DataFrameSelector(BaseEstimator, TransformerMixin):
+    def __init__(self,attribute_names,a,b):
+        self.attribute_names = attribute_names
+        self.a = a
+        self.b = b
+    def fit(self, X,y=None):
+        return self
+    def transform(self, X, y=None):
+        return X[self.attribute_names].values
+    
 #create an app
 app = Flask(__name__)
 #load the model
