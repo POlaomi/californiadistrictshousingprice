36561feba61ab6dba1c48d07782fa5337903b477
Peter Olaomi <p.olaomi@gmail.com>
2023-04-05 23:35:41 -0400

    Cleaning up

--- app.py
+++ app.py
@@ -5,40 +5,10 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from sklearn.preprocessing import OneHotEncoder
-#from django.shortcuts import render
-
-from sklearn.base import BaseEstimator, TransformerMixin 
-    
-class AttributesAdder(BaseEstimator, TransformerMixin):
-    def __init__(self,add_bedrooms_per_room=True):
-        self.add_bedrooms_per_room = add_bedrooms_per_room
-    def fit(self, X, y=None):
-        return self
-    def transform(self, X, y=None):
-        rooms_per_household = X[:, 3] / X[:,6]
-        if self.add_bedrooms_per_room:
-            bedrooms_per_room = X[:,4]/ X[:,3]
-            return np.c_[X,rooms_per_household, bedrooms_per_room]
-        else:
-            return np.c_[X,rooms_per_household]
-class CategoricalEncoder(BaseEstimator, TransformerMixin):
-    def __init__(self, ohe = OneHotEncoder()):
-        self.ohe = ohe
-    def fit(self, X,y=None,):
-        return self.ohe.fit(X)
-    def transform(self, X, y=None):
-        return self.ohe.transform(X).toarray()
-    
-class DataFrameSelector(BaseEstimator, TransformerMixin):
-    def __init__(self,attribute_names,a,b):
-        self.attribute_names = attribute_names
-        self.a = a
-        self.b = b
-    def fit(self, X,y=None):
-        return self
-    def transform(self, X, y=None):
-        return X[self.attribute_names].values
-
+from sklearn.base import BaseEstimator, TransformerMixin
+from attributes_adder import AttributesAdder
+from categorical_encoder import CategoricalEncoder
+from dataframe_selector import DataFrameSelector
 import __main__
 __main__.DataFrameSelector = DataFrameSelector
 __main__.CategoricalEncoder = CategoricalEncoder
